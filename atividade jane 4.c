f47de6dd3583f60fe0cf18ef783829f2e6688ff4
Pedro augusto borges de sá <Pedroaugusto.borges36@gmail.com>
2025-03-25 20:12:10 -0300

    Média aritmética em c

---
+++ atividade jane 4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+main()
+
+
+
+{   //Base do codigo
+    float x,y,z,soma;
+
+    printf("Digite um valor: ");
+    scanf("%f",&x);
+
+    printf("\n\n");
+
+    printf("Digite um valor: ");
+    scanf("%f",&y);
+
+    printf("\n\n");
+
+    printf("Digite um valor: ");
+    scanf("%f",&z);
+
+    printf("\n\n");
+
+
+    //Calculo do codigo
+
+    soma=x+y+z/3;
+
+    //Final do codigo
+
+    printf("A media aritmetica dos valores sao: %.2f",soma);
+
+
+    printf("\n\n");
+
+
+    return 0;
+
+
+
+
+}
